89127a8c1afba7b3f1f2bcca0546bd0470e42229
EMILIANO JONAS GOROJOD <49929524@ort>
2026-05-12 09:07:10 -0300

    a

---
+++ Models/SugeridorReceta.cs
@@ -0,0 +1,19 @@
+namespace TP03_Waserman.Gorojod.Models;
+
+public class sugeridorReceta{
+    public string nombre;
+    public dateTime fechaNacimiento;
+    public bool tipo;
+    public int presupuesto;
+    public int cantidad;
+
+    public SugeridorReceta (){
+
+    }
+
+    public SugeridosReceta(string nombre, dateTime fechaNacimiento, bool tipo, int presupuesto, int cantidad){
+        nombre = this.nombre;
+        fechaNacimiento = this.fechaNacimiento;
+        tioi
+    }
+}
