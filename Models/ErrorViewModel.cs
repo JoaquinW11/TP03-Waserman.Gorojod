0153ce7427587a2035ba3fcdac3fa49410a855cc
EMILIANO JONAS GOROJOD <49929524@ort>
2026-05-12 08:55:19 -0300

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP03_Waserman.Gorojod.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
